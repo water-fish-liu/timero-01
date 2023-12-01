5027c6916fcdcaca0a432416ef2076e05e008714
User <User@DESKTOP-H3AD42Q>
2023-12-01 20:10:52 +0800

    添加项目文件。

---
+++ timero/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace CountdownTimerApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.numericUpDownHours = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownMinutes = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownHours
+            // 
+            this.numericUpDownHours.BackColor = System.Drawing.Color.White;
+            this.numericUpDownHours.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.numericUpDownHours.Location = new System.Drawing.Point(40, 105);
+            this.numericUpDownHours.Name = "numericUpDownHours";
+            this.numericUpDownHours.Size = new System.Drawing.Size(47, 29);
+            this.numericUpDownHours.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "开始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("黑体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(45, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "  倒计时  ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "power by c#,made by lwen";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // numericUpDownMinutes
+            // 
+            this.numericUpDownMinutes.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.numericUpDownMinutes.Location = new System.Drawing.Point(105, 105);
+            this.numericUpDownMinutes.Name = "numericUpDownMinutes";
+            this.numericUpDownMinutes.Size = new System.Drawing.Size(75, 29);
+            this.numericUpDownMinutes.TabIndex = 4;
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(201, 105);
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(47, 29);
+            this.numericUpDownSeconds.TabIndex = 5;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(173, 157);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 6;
+            this.stopButton.Text = "停止";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "时";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "分";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "秒";
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.numericUpDownSeconds);
+            this.Controls.Add(this.numericUpDownMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDownHours);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Genshin impact";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownHours;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownMinutes;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ timero/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+
+namespace CountdownTimerApp
+{
+    public partial class MainForm : Form
+    {
+        private int secondsRemaining;
+        private SoundPlayer soundPlayer;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeTimer();
+            InitializeSoundPlayer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer1.Interval = 1000; // 1
+            timer1.Tick += Timer1_Tick;
+        }
+        private void InitializeSoundPlayer()
+        {
+            // 初始化 
+            soundPlayer = new SoundPlayer(".\\file.wav");
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (secondsRemaining > 0)
+            {
+                secondsRemaining--;
+                UpdateDisplay();
+            }
+            else
+            {
+                timer1.Stop();
+                PlayAudio();
+                MessageBox.Show("倒计时结束！");
+                label1.Text = $"  已结束  ";
+                DisPlayAudio();
+
+                //计算式
+            }
+        }
+
+        private void PlayAudio()
+        {
+            // 播
+            soundPlayer.Play();
+        }
+        private void DisPlayAudio()
+        {
+            //停
+            soundPlayer.Stop();
+        }
+
+        private void UpdateDisplay()
+        {
+            label1.Text = $"还剩{secondsRemaining}秒";
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 时分秒控件的值
+                int hours = (int)numericUpDownHours.Value;
+                int minutes = (int)numericUpDownMinutes.Value;
+                int seconds = (int)numericUpDownSeconds.Value;
+
+                // 防呆傻
+                if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59)
+                {
+                    MessageBox.Show("请输入有效的时间范围。");
+                    return; // 拒绝
+                }
+
+                // 将时分秒转化为秒
+                int s = hours * 3600 + minutes * 60 + seconds;
+                secondsRemaining = s;
+                UpdateDisplay();
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"发生错误：{ex.Message}");
+                // 实际用不到
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            //终止按钮
+            timer1.Stop();
+            MessageBox.Show("倒计时已终止！");
+            label1.Text = $"  已终止  ";
+        }
+    }
+}
